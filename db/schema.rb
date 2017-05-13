775cf16a2fa109901c0b3157b935bd1af916cc80
SKmtt <matej.kaduk@student.tuke.sk>
2017-05-13 18:06:22 +0200

    daco nove

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513074836) do
+ActiveRecord::Schema.define(version: 20170513091608) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id_id"], name: "index_orders_on_user_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
